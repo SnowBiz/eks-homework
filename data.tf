69012480caa79ac445ef31d6ef62ea46c1709482
alansnow <mail@alansnow.dev>
2023-06-07 14:37:56 -0400

    Removing data source that was no longer needed.

--- data.tf
+++ data.tf
@@ -1,15 +1,9 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
 data "aws_eks_cluster_auth" "this" {
   name = var.cluster_name
 }
-
 data "aws_ecrpublic_authorization_token" "token" {
   provider = aws.virginia
 }
